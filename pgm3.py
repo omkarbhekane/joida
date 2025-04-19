6ea35c98e6000b94fb2fcc0944ba9a70eb9d342a
Omkar Bhekane <168728239+omkarbhekane@users.noreply.github.com>
2025-04-19 10:47:03 +0530

    Add files via upload

---
+++ pgm3.py
@@ -0,0 +1,23 @@
+import matplotlib
+import matplotlib.pyplot as plt 
+matplotlib.use("TkAgg")
+import math
+def linear_search(list1,n,key):
+    #searching list1 sequentially
+    for i in range(0,n+1):
+        if(list1[i]==key):
+            return i
+    return -1
+thevalues=[10,20,30,40,50,60,70]
+target=int(input("enter key to search:"))
+tmp=linear_search(thevalues,len(thevalues)-1,target)
+if tmp==1:
+    print("target value not found in list")
+else:
+    print("target value found in list at location",tmp)
+x=list(range(1,1000))
+plt.plot(x,x)
+plt.title("linear-time complexity is O(log n)")
+plt.xlabel("input")
+plt.ylabel("time")
+plt.show()
